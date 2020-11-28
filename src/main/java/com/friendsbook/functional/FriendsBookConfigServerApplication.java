011c04763d23ed3ce9c7d0058eaff2a0f95a9311
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-28 13:32:52 +0530

    made config a functional microservice

--- src/main/java/com/friendsbook/functional/FriendsBookConfigServerApplication.java
+++ src/main/java/com/friendsbook/functional/FriendsBookConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.friendsbook.configserver;
+package com.friendsbook.functional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
