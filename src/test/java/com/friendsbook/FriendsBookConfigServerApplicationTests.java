19c10f7c8dd2537cca72b9a0643d3d55679cf67b
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2020-11-28 11:42:07 +0530

    Initial commit

---
+++ src/test/java/com/friendsbook/FriendsBookConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsBookConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
